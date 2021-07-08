c856b77cc47f213f4cc27ca594f097de5653f3f6
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-08 11:14:11 +0200

    slight correction

--- GLDM_U1.java
+++ GLDM_U1.java
@@ -18,7 +18,7 @@ public class GLDM_U1 implements PlugIn {
 		ImageJ ij = new ImageJ(); // neue ImageJ Instanz starten und anzeigen
 		ij.exitWhenQuitting(true);
 
-		GLDM_U1 imageGeneration = new GLDM_U1_s0577683();
+		GLDM_U1 imageGeneration = new GLDM_U1();
 		imageGeneration.run("");
 	}
 
@@ -82,7 +82,7 @@ public class GLDM_U1 implements PlugIn {
 				int pos = y * width + x; // Arrayposition bestimmen
 				/* Da alle Werte 0 sind, besteht kein Grund sie hier
 				 * in einzelne Variablen zu speichern. Diese Werte 
-				 * werden nicht weiter ver‰ndert, daher unnˆtig
+				 * werden nicht weiter ver√§ndert, daher unn√∂tig
 				 * hier Variablen zu nutzen und vorzuhalten. Redundanz.*/
 				 // Werte zurueckschreiben
 				pixels[pos] = 0xFF000000 | (0 << 16) | (0 << 8) | 0;
@@ -103,7 +103,7 @@ public class GLDM_U1 implements PlugIn {
 		}
 	}
 
-	//Farbcodes gem‰ﬂ https://www.schemecolor.com/belgium-flag-colors.php
+	//Farbcodes gem√§√ü https://www.schemecolor.com/belgium-flag-colors.php
 	private void generateBelgianFlag(int width, int height, int[] pixels) {
 		// Schleife ueber die y-Werte
 		for (int y = 0; y < height; y++) {
@@ -113,16 +113,16 @@ public class GLDM_U1 implements PlugIn {
 				
 				if (x < width / 3) {
 					/* Wenn der Pixel im ersten vertikalen Drittel ist
-					 * (also links), dann schwarz einf‰rben.*/
+					 * (also links), dann schwarz einf√§rben.*/
 					pixels[pos] = 0xFF000000 | (0 << 16) | (0 << 8) | 0;
 				
 				} else if (x > (width / 3) * 2) {
 					/* Wenn der Pixel im letzten vertikalen Drittel ist
-					 * (also rechts), dann rot einf‰rben.*/
+					 * (also rechts), dann rot einf√§rben.*/
 					pixels[pos] = 0xFF000000 | (239 << 16) | (51 << 8) | 64;
 
 				} else {
-					//Ansonsten den Rest (Mitte) gelb f‰rben.
+					//Ansonsten den Rest (Mitte) gelb f√§rben.
 					pixels[pos] = 0xFF000000 | (253 << 16) | (218 << 8) | 36;
 
 				}
@@ -137,9 +137,9 @@ public class GLDM_U1 implements PlugIn {
 			for (int x = 0; x < width; x++) {
 				int pos = y * width + x; // Arrayposition bestimmen
 				/* Die x Werte mit 1.0 multiplizieren,
-				 * um Double-Werte zu erzeugen, f¸r eine pr‰zise
+				 * um Double-Werte zu erzeugen, f√ºr eine pr√§zise
 				 * Berechnung der Mischfarbwerte.
-				 * Horizontalwerte in Verh‰ltnis zur Breite setzen.*/
+				 * Horizontalwerte in Verh√§ltnis zur Breite setzen.*/
 				int val = (int) ((1.0 * x / width) * 255);
 				// Die Double werte in Integer casten und darstellen.
 				pixels[pos] = 0xFF000000 | (val << 16) | (val << 8) | val;
@@ -154,11 +154,11 @@ public class GLDM_U1 implements PlugIn {
 			for (int x = 0; x < width; x++) {
 				int pos = y * width + x; // Arrayposition bestimmen
 				/* jeweiligen x & y Wert mit 1.0 multiplizieren,
-				 * um Double-Werte zu erzeugen, f¸r eine pr‰zise
+				 * um Double-Werte zu erzeugen, f√ºr eine pr√§zise
 				 * Berechnung der Mischfarbwerte.*/
 				int red = (int) ((1.0 * x / width) * 255);
 				
-				/* Da es kein Gr¸n gibt, wird es nicht
+				/* Da es kein Gr√ºn gibt, wird es nicht
 				 * berechnet, nur auf 0 gesetzt*/
 				int blue = (int) ((1.0 * y / height) * 255);
 				// Die Double werte in Integer casten und darstellen.
@@ -167,9 +167,9 @@ public class GLDM_U1 implements PlugIn {
 		}
 	}
 
-	// Farbcodes gem‰ﬂ https://www.schemecolor.com/united-states-of-america-flag-colors.php
+	// Farbcodes gem√§√ü https://www.schemecolor.com/united-states-of-america-flag-colors.php
 	private void generateUSAFlag(int width, int height, int[] pixels) {
-		// Hˆhe eines Streifens als Double berechnen; 13tel der Gesamthˆhe
+		// H√∂he eines Streifens als Double berechnen; 13tel der Gesamth√∂he
 		double stripes = (height / 13.0);
 		// Schleife ueber die y-Werte
 		for (int y = 0; y < height; y++) {
@@ -177,13 +177,13 @@ public class GLDM_U1 implements PlugIn {
 			for (int x = 0; x < width; x++) {
 				int pos = y * width + x; // Arrayposition bestimmen
 				
-				/* Wenn aktueller Pixel hˆher ist als der 8. Streifen
+				/* Wenn aktueller Pixel h√∂her ist als der 8. Streifen
 				 * bzw weiter links als 40% der Gesamtbreite, dann
-				 * wird der Bereich gem‰ﬂ den Standardmaﬂen 
-				 * in der Frabe "American Blue" gef‰rbt.*/
+				 * wird der Bereich gem√§√ü den Standardma√üen 
+				 * in der Frabe "American Blue" gef√§rbt.*/
 				if (1.0 * y < (stripes) * 7 & x < width * 0.4) {
 					/* Da ein Streifen eigentlich 30,7px hoch sind, noch 1.0 * dazu
-					 * im eine Typenumwandlung vorzunehmen, f¸r pr‰zise Rechnung.*/
+					 * im eine Typenumwandlung vorzunehmen, f√ºr pr√§zise Rechnung.*/
 					pixels[pos] = 0xFF000000 | (60 << 16) | (59 << 8) | 110;
 
 				} else {
@@ -208,8 +208,8 @@ public class GLDM_U1 implements PlugIn {
 				int pos = y * width + x; // Arrayposition bestimmen
 				// Wenn der aktuelle Pixel oberhalb der Mitte liegt
 				if (y < height / 2) {
-					// Pixel wird nur ausgef¸llt, wenn die x-Position
-					 // den y-Wert nicht ¸bersteigt, damit wird eine
+					// Pixel wird nur ausgef√ºllt, wenn die x-Position
+					 // den y-Wert nicht √ºbersteigt, damit wird eine
 					 // Diagonale erzeugt.
 					if (x < y) {
 
@@ -231,39 +231,40 @@ public class GLDM_U1 implements PlugIn {
 		}
 	}*/
 
-	// Farbcodes gem‰ﬂ https://www.schemecolor.com/czech-republic-flag-colors.php
+	// Farbcodes gem√§√ü https://www.schemecolor.com/czech-republic-flag-colors.php
 	private void generateCzechFlag(int width, int height, int[] pixels) {
-		// Seitenverh‰ltnis der Flagge berechnen.
+		// Seitenverh√§ltnis der Flagge berechnen.
 		double ratio = (width * 1.0) / height;
 		// Schleife ueber die y-Werte
 		for (int y = 0; y < height; y++) {
 			// Schleife ueber die x-Werte
 			for (int x = 0; x < width; x++) {
 				int pos = y * width + x; // Arrayposition bestimmen
-				// Obere h‰lfte der Flagge zeichnen
+				// Obere h√§lfte der Flagge zeichnen
 				if (y < height / 2) {
 					// Wenn der X-Wert neidriger ist als der Y-Wert
 					// setze Pixel, dabei wird die Diagonale erzeugt.
 					if (x < y * ratio) {
-						// Der obere linke Dreieck-Teil wird blau gef‰rbt.
+						// Der obere linke Dreieck-Teil wird blau gef√§rbt.
 						pixels[pos] = 0xFF000000 | (17 << 16) | (69 << 8) | 126;
 					} else {
-						// Ansonsten wird die obere H‰lfte weiﬂ gef‰rbt.
+						// Ansonsten wird die obere H√§lfte wei√ü gef√§rbt.
 						pixels[pos] = 0xFF000000 | (255 << 16) | (255 << 8) | 255;
 					}
 				} else {
 					if (x < width - y * ratio) {
-						// Der untere linke Dreieck-Teil wird blau gef‰rbt.
+						// Der untere linke Dreieck-Teil wird blau gef√§rbt.
 						pixels[pos] = 0xFF000000 | (17 << 16) | (69 << 8) | 126;
 					} else {
-						// Ansonsten wird die untere H‰lfte rot gef‰rbt.
+						// Ansonsten wird die untere H√§lfte rot gef√§rbt.
 						pixels[pos] = 0xFF000000 | (215 << 16) | (20 << 8) | 26;
 					}
 				}
 			}
 		}
 	}
-	//My Codes end here. Made by Nermin Rustic :)
+	//My Codes end here. Made byn-c0de-r :)
+	
 	private void dialog() {
 		// Dialog fuer Auswahl der Bilderzeugung
 		GenericDialog gd = new GenericDialog("Bildart");
